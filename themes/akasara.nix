4030949770cd3fd96329bee3bf46e5b72f108ce8
Bartosz Szewczyk <cheryllamb123098@protonmail.com>
2025-12-26 03:01:01 +0000

    added a first theme, for now it has placeholder colours

---
+++ themes/akasara.nix
@@ -0,0 +1,54 @@
+{
+  name = "Akasara";
+  
+  colors = {
+    bg = "#FF0000"; #TEST
+    bgAlt = "#00FF00"; #TEST
+    fg = "#0000FF"; #TEST
+    fgAlt = "#FFFF00"; #TEST
+    
+    # Accents
+    primary = "#FF00FF"; #TEST
+    secondary = "#00FFFF"; #TEST
+    
+    # Semantic
+    success = "#00FF00"; #TEST
+    warning = "#FFA500"; #TEST
+    error = "#FF0000"; #TEST
+    info = "#0000FF"; #TEST
+    
+    # UI
+    border = "#FF00FF"; #TEST
+    selection = "#FFFF0088"; #TEST
+    hover = "#00FFFF88"; #TEST
+    
+    terminal = {
+      black = "#000000"; #TEST
+      red = "#FF0000"; #TEST
+      green = "#00FF00"; #TEST
+      yellow = "#FFFF00"; #TEST
+      blue = "#0000FF"; #TEST
+      magenta = "#FF00FF"; #TEST
+      cyan = "#00FFFF"; #TEST
+      white = "#FFFFFF"; #TEST
+       #TEST
+      brightBlack = "#888888"; #TEST
+      brightRed = "#FF8888"; #TEST
+      brightGreen = "#88FF88"; #TEST
+      brightYellow = "#FFFF88"; #TEST
+      brightBlue = "#8888FF"; #TEST
+      brightMagenta = "#FF88FF"; #TEST
+      brightCyan = "#88FFFF"; #TEST
+      brightWhite = "#FFFFFF"; #TEST
+    };
+  };
+  
+  fonts = {
+    main = "JetBrainsMono Nerd Font";
+    mono = "JetBrainsMono Nerd Font Mono";
+    size = 11;
+  };
+  
+  cursor = "Bibata-Modern-Classic";
+  icon = "Papirus-Dark";
+}
