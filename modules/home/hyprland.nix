ba3d25085743ac638518216c8745c5a8439d2941
Bartosz Szewczyk <cheryllamb123098@protonmail.com>
2025-12-26 05:03:27 +0000

    huh

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -4,13 +4,12 @@
     enable = true;
     settings = {
       bind = [
-        "$mainMod, Return exec, $terminal"
+        "$mainMod, Return, exec, kitty"
         "$mainMod SHIFT, Q, killactive"
         "$mainMod SHIFT, O, exit"
       ];
       general = {
         "$mainMod" = "ALT";
-        "$terminal" = "kitty";
         "col.active_border" = "rgb(${builtins.substring 1 (-1) selectedTheme.colours.primary})";
         "col.inactive_border" = "rgb(${builtins.substring 1 (-1) selectedTheme.colours.secondary})";
       };
