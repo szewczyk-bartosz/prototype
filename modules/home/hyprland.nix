9907c6963480e85e51a5129bb732a766e7f2bfb5
Bartosz Szewczyk <cheryllamb123098@protonmail.com>
2025-12-26 05:08:33 +0000

    some more hyprland config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,4 +1,7 @@
 { selectedTheme, ... }:
+let
+  workspaces = builtins.genList (x: x) 10;
+in 
 {
   wayland.windowManager.hyprland = {
     enable = true;
@@ -7,7 +10,13 @@
         "$mainMod, Return, exec, kitty"
         "$mainMod SHIFT, Q, killactive"
         "$mainMod SHIFT, O, exit"
+        "$mainMod, H, movefocus, l"
+        "$mainMod, J, movefocus, d"
+        "$mainMod, K, movefocus, u"
+        "$mainMod, L, movefocus, r"
       ];
+      ++ (map (i: "SUPER, ${toString i}, workspace, ${toString i}") workspaces)
+      ++ (map (i: "SUPER SHIFT, ${toString i}, movetoworkspace, ${toString i}") workspaces);
       general = {
         "$mainMod" = "ALT";
         "col.active_border" = "rgb(${builtins.substring 1 (-1) selectedTheme.colours.primary})";
